8d4e72d6cc3587902f289105bae07b171a598ad0
Divikiran <divikiran1@gmail.com>
2016-04-15 17:23:08 +1000

    floor checkin

---
+++ ElevatorSample/ElevatorSample/Controls/Floor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace ElevatorSample
+{
+	public class Floor : StackLayout
+	{
+		public int FloorNumber {
+			get;
+			set;
+		}
+		public Floor (int floorNumber)
+		{
+			FloorNumber = floorNumber;
+			this.VerticalOptions = LayoutOptions.Center;
+			this.HorizontalOptions = LayoutOptions.FillAndExpand;
+
+			this.Children.Add 
+			(
+				new Label () {
+					Text = FloorNumber.ToString (),
+					TextColor = Color.White,
+					BackgroundColor = Color.Blue,
+					HeightRequest = 30,
+					WidthRequest = 30,
+					VerticalOptions = LayoutOptions.Center,
+					HorizontalOptions = LayoutOptions.Center
+				});
+		}
+	}
+}
+
---
+++ ElevatorSample/ElevatorSample/Pages/ElevatorPage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Xamarin.Forms;
+using System.Collections.Generic;
+
+namespace ElevatorSample
+{
+	public class ElevatorPage : ContentPage
+	{
+		public ElevatorPage ()
+		{
+			Title = "Elevator";
+
+			var contentStack = new StackLayout (){
+				VerticalOptions=LayoutOptions.FillAndExpand,
+				HorizontalOptions=LayoutOptions.FillAndExpand
+			};
+
+			List<Floor> floors = new List<Floor> ();
+			for(int i = 10; i >=1; i--)
+			{
+				contentStack.Children.Add(new Floor(i));
+			}
+
+
+			Content = contentStack;
+		}
+	}
+}
+
+
